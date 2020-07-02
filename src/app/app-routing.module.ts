b4d4d41aeaa59caaca378b8d6759e527d32b713d
Venu Gopal Boddu <vb19419@corp.ojas-it.com>
2020-07-02 18:37:31 +0530

    media

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+  {path: '', component: RegistrationComponent},
+  {path: 'reg', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
